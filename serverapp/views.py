ff49786c32103293e742e81ae1fe72834bf25c14
paschal-ike <paschalpaul268@gmail.com>
2024-07-02 10:22:57 +0100

    'refacto'

--- serverapp/views.py
+++ serverapp/views.py
@@ -1,28 +1,28 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 import requests
 
+
+
+@api_view(['GET'])
 def hello(request):
     visitor_name = request.GET.get('visitor_name', 'Guest')
-    client_ip = request.META.get('REMOTE_ADDR', '127.0.0.1')
-
-    # Hardcoding an example IP for demonstration purposes
-    if client_ip == '127.0.0.1':
-        client_ip = '8.8.8.8'  # Example IP address (Google's public DNS server)
-
-    # Get the location based on IP address
-    location_response = requests.get(f"https://ipinfo.io/{client_ip}/json")
-    location_data = location_response.json()
-    city = location_data.get('city', 'Unknown')
-
-    # For simplicity, use a fixed temperature. Ideally, use a weather API.
-    temperature = 11
-
-    greeting = f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {city}"
-
-    response_data = {
-        "client_ip": client_ip,
-        "location": city,
-        "greeting": greeting
-    }
-
-    return JsonResponse(response_data)
+    client_ip = request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('REMOTE_ADDR', ''))
+    
+    # Get location and weather data from OpenWeatherMap
+    weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=f112e3200561aa905c68fc8aca47be38&units=metric'
+    weather_response = requests.get(weather_url)
+    
+    if weather_response.status_code == 200:
+        weather_data = weather_response.json()
+        city = weather_data['name']
+        temperature = weather_data['main']['temp']
+    else:
+        city = 'Unknown'
+        temperature = 'Unknown'
+    
+    return Response({
+        'client_ip': client_ip,
+        'location': city,
+        'greeting': f"Hello, {visitor_name}! The temperature is {temperature} degrees Celsius in {city}"
+    })
